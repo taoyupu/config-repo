b934219d307f3716df988b2a29d446804c30cd02
陶玉璞 <1094991018@qq.com>
2020-03-18 13:43:14 +0800

    mtest

---
+++ src/main/java/com/typ/configrepo/ConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package com.typ.configrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigRepoApplication.class, args);
+    }
+
+}
